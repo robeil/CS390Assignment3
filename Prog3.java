e24f5a1a5cc59c3e8e3ea13cfa77dbdc8c0fb512
Robeil Aregawi <silukeen1@gmail.com>
2022-02-25 22:16:48 -0600

    Solved Prog3

---
+++ Prog3.java
@@ -0,0 +1,34 @@
+package CS390Asssignment3;
+
+import java.util.Arrays;
+
+public class Prog3 {
+
+    public static int binarySearch(int[] array, int left, int right, int target){
+
+        //sorting the array first
+       Arrays.sort(array);
+
+       // storing the middle index
+       int middleArray = left + (right - left) /2;
+
+        if(array.length == 0){
+            return -1;
+        } else {
+            if(target == array[middleArray]){
+                return array[middleArray];
+            }
+            if(target < array[middleArray]){
+                return binarySearch(array,left, middleArray, target);
+            } else {
+               return binarySearch(array,middleArray, right, target);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        int[]array = {2,6,8,9,5,4,1,3,11};
+
+       System.out.println(binarySearch(array,0,array.length-1,9));
+    }
+}

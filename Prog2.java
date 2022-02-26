a2864d87828c49c4da291516e0d7f40c53699bb5
Robeil Aregawi <silukeen1@gmail.com>
2022-02-25 21:19:53 -0600

    solved prog2

---
+++ Prog2.java
@@ -0,0 +1,25 @@
+package CS390Asssignment3;
+
+public class Prog2 {
+
+    public static char minCharacter(String str,int index){
+        //assigning the min to the largest ASCII character which is ~ = 126
+       char min = '~';
+        if(index < str.length()) {
+            if(str.charAt(index) < min){
+                min = str.charAt(index);
+            }
+            char nextMin = minCharacter(str,index + 1);
+            if(nextMin < min){
+                min = nextMin;
+            }
+        };
+        return min;
+    }
+
+    public static void main(String[] args){
+
+   System.out.println(minCharacter("akel",0));
+
+    }
+}
